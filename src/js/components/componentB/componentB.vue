1b283c959e840da0c1170f8d133e2bc5586c1794
Joseph Pal <joe.pal@gmx.net>
2019-11-23 23:48:57 +0200

    added konva draw on canvas example

--- src/js/components/componentB/componentB.vue
+++ src/js/components/componentB/componentB.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="componentB">
-    <div class="content" @click="clicked"></div>
-  </div>
+    <div class="componentB">
+        <div class="content">
+          <v-stage :config="configKonva" id="container">
+              <v-layer>
+                  <!--<v-circle :config="configCircle"></v-circle> #Method 1 -->
+              </v-layer>
+          </v-stage>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,7 +17,18 @@
 
     data() {
       return {
-
+        configKonva: {
+          width: 500,
+          height: 500
+        },
+        configCircle: {
+          x: 100,
+          y: 100,
+          radius: 70,
+          fill: "red",
+          stroke: "black",
+          strokeWidth: 4
+        }
       };
     },
 
@@ -20,7 +37,7 @@
     },
 
     mounted() {
-
+      this.drawCircle();
     },
 
     computed: {
@@ -28,9 +45,38 @@
     },
 
     methods: {
-        clicked() {
-          console.log("componentB clicked!");
-        }
+      drawCircle() {
+        console.log("drawing circle ...");
+
+        // first we need to create a stage
+        var stage = new Konva.Stage({
+          container: 'container',   // id of container <div>
+          width: 500,
+          height: 500
+        });
+
+        // then create layer
+        var layer = new Konva.Layer();
+
+        // create our shape
+        var circle = new Konva.Circle({
+          x: stage.width() / 2,
+          y: stage.height() / 2,
+          radius: 70,
+          fill: 'red',
+          stroke: 'black',
+          strokeWidth: 4
+        });
+
+        // add the shape to the layer
+        layer.add(circle);
+
+        // add the layer to the stage
+        stage.add(layer);
+
+        // draw the image
+        layer.draw();
+      }
     },
 
     components: {
@@ -55,8 +101,6 @@
       align-items: center;
       justify-content: center;
       box-shadow: 2px 2px 4px $colorDarkGrey;
-
-      color: #263238;
       height: 100%;
 
       cursor: pointer;
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App';
+
 import Vuebar from 'vuebar';
+import VueKonva from 'vue-konva'
 
 import '../assets/fonts/roboto.css';
 import '../scss/app.scss';
@@ -8,6 +10,7 @@ import '../scss/app.scss';
 Vue.config.productionTip = false;
 
 Vue.use(Vuebar);
+Vue.use(VueKonva);
 
 /* eslint-disable no-new */
 new Vue({
