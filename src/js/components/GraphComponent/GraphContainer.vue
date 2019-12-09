67909f0de29deaf9dbb94c9e00ef6732c457eafe
Joseph Pal <joe.pal@gmx.net>
2019-12-09 19:18:36 +0200

    line chart label fixes, implemented websocket methods

--- src/js/components/GraphComponent/GraphContainer.vue
+++ src/js/components/GraphComponent/GraphContainer.vue
@@ -3,20 +3,30 @@
         <div class="content">
             <div class="button-container">
                 <div class="buttons">
-                  <RectangleButton  class=""
+                  <RectangleButton  class="connect-button"
+                                    :icon="connectButtonIcon"
+                                    :enabled="!webSocketConnecting"
+                                    @click="onConnectButtonClicked"
+                                    :showSpinner="showConnectBtnSpinner" />
+                  <RectangleButton  class="save-button"
                                     icon="save"
-                                    :enabled="true"
-                                    @click=""
+                                    :enabled="webSocketReady"
+                                    @click="onSaveButtonClicked"
                                     :showSpinner="false" />
-                  <RectangleButton  class=""
+                  <RectangleButton  class="load-button"
                                     icon="load"
-                                    :enabled="true"
+                                    :enabled="webSocketReady"
                                     @click="emitPPMData"
                                     :showSpinner="false" />
-                  <RectangleButton  class=""
-                                    icon="arrow"
-                                    :enabled="true"
-                                    @click=""
+                  <RectangleButton  class="clear-button"
+                                    icon="clear"
+                                    :enabled="webSocketReady"
+                                    @click="onClearButtonClicked"
+                                    :showSpinner="false" />
+                  <RectangleButton  class="reboot-button"
+                                    icon="reload"
+                                    :enabled="webSocketReady"
+                                    @click="onRebootButtonClicked"
                                     :showSpinner="false" />
                 </div>
             </div>
@@ -25,12 +35,20 @@
                 <div class='row'>
                     <div class='column'>
                         <div class='item'>
-                            <line-chart :chart-data="datacollection1" class="chart" style="height: 95%; width: 95%;"></line-chart>
+                            <line-chart :chart-data="datacollection1"
+                                        xAxisLabel="number of threads"
+                                        yAxisLabel="time  t{p}  /  ms"
+                                        class="chart"
+                                        style="height: 95%; width: 95%;" />
                         </div>
                     </div>
                     <div class='column'>
                         <div class='item'>
-                            <line-chart :chart-data="datacollection2" class="chart" style="height: 95%; width: 95%;"></line-chart>
+                            <line-chart :chart-data="datacollection2"
+                                        xAxisLabel="number of threads"
+                                        yAxisLabel="Speedup  S  =  t{1}  /  t{p}"
+                                        class="chart"
+                                        style="height: 95%; width: 95%;" />
                         </div>
                     </div>
                 </div>
@@ -38,12 +56,20 @@
                 <div class='row'>
                     <div class='column'>
                         <div class='item'>
-                            <line-chart :chart-data="datacollection3" class="chart" style="height: 95%; width: 95%;"></line-chart>
+                            <line-chart :chart-data="datacollection3"
+                                        xAxisLabel="number of threads"
+                                        yAxisLabel="time  t{p}  /  ms"
+                                        class="chart"
+                                        style="height: 95%; width: 95%;" />
                         </div>
                     </div>
                     <div class='column'>
                         <div class='item'>
-                            <line-chart :chart-data="datacollection4" class="chart" style="height: 95%; width: 95%;"></line-chart>
+                            <line-chart :chart-data="datacollection4"
+                                        xAxisLabel="number of threads"
+                                        yAxisLabel="Speedup  S  =  t{1}  /  t{p}"
+                                        class="chart"
+                                        style="height: 95%; width: 95%;" />
                         </div>
                     </div>
                 </div>
@@ -53,11 +79,17 @@
 </template>
 
 <script>
+  /* as an example of emitting the received compressed *.ppm file */
+  import { mandelbrot } from '../../utils/MandelbrotExample';
+
   import LineChart from './LineChart.js'
   import RectangleButton from '../buttons/RectangleButton'
+  import saveAs from '../../utils/FileSaver';
 
-  /* as an example of emitting the received ppm file */
-  import { mandelbrot } from '../../utils/MandelbrotExample';
+  /* websocket connection javascript files */
+  import SocketMessages from '../../utils/websocket/SocketMessageTypes';
+  import socketConnector from '../../utils/websocket/socketConnector';
+  import { validateIp, isEmpty } from '../../utils/websocket/validationUtils'
 
   export default {
     name: 'GraphContainer',
@@ -82,18 +114,38 @@
 
         /* values */
         ppmImageData: "",
+
+        /* websocket */
+        ip: "192.168.4.1",
+        webSocketReady: false,
+        webSocketConnecting: false,
+
+        /* button spinner state */
+        showConnectBtnSpinner: false,
+        showSaveBtnSpinner: false,
+        showLoadBtnSpinner: false,
+        showClearBtnSpinner: false,
+        showRebootBtnSpinner: false,
       };
     },
 
     mounted() {
+      /* default websocket function call if connection is closed, open ore a new message is received */
+      socketConnector.onClose(this.onSocketClose);
+      socketConnector.onOpen(this.onSocketOpen);
+      socketConnector.onMessage(this.onSocketMessage);
+
+      /* render line charts with default settings  */
       this.fillData();
 
-      this.interval = setInterval(() => {
+      /* test routine to add random data to the line charts every 500ms */
+      /*this.interval = setInterval(() => {
           this.addData();
-      }, 500);
+      }, 500);*/
     },
 
     methods: {
+      /* line chart functions */
       addData() {
         this.index = this.index + 1;
         this.labelArray.push(this.index);
@@ -120,7 +172,7 @@
           labels: this.labelArray,
           datasets: [
             {
-              label: "data series 1",
+              label: "execution time [Computation]",
               backgroundColor: "#f87979",
               data: this.dataArray1
             }
@@ -131,7 +183,7 @@
           labels: this.labelArray,
           datasets: [
             {
-              label: "data series 2",
+              label: "speedup factor [Computation]",
               backgroundColor: "#41b883",
               data: this.dataArray2
             }
@@ -142,7 +194,7 @@
           labels: this.labelArray,
           datasets: [
             {
-              label: "data series 3",
+              label: "execution time [Mandelbrot]",
               backgroundColor: "#303f9f",
               data: this.dataArray3
             }
@@ -153,12 +205,86 @@
           labels: this.labelArray,
           datasets: [
             {
-              label: "data series 4",
+              label: "speedup factor [Mandelbrot]",
               backgroundColor: "#35495e",
               data: this.dataArray4
             }
           ]
         };
+      },
+
+
+      /* websocket functions  */
+      onSocketClose() {
+        this.webSocketReady = false;
+      },
+
+      onSocketOpen() {
+        this.webSocketReady = true;
+      },
+
+      onSocketMessage(message) {
+        //console.warn(message);
+        console.log(`Received message: ${message}`);
+        switch (message) {
+          case SocketMessages.RUNNING:
+            this.isReady = true;
+            this.isRunning = true;
+            this.isPaused = false;
+            break;
+          case SocketMessages.READY:
+            this.isReady = true;
+            break;
+          case SocketMessages.ERROR:
+            console.error('Server error');
+            break;
+          default:
+            console.warn('Unknown socket message');
+            break;
+        }
+      },
+
+
+      /* button clicked functions */
+      onConnectButtonClicked() {
+        if( !this.webSocketReady ) {
+          if( !validateIp(this.ip) ) {
+            /* IP adress is not valid */
+            console.log("Invalid IP address!");
+            this.webSocketConnecting = false;
+          } else {
+            if( this.webSocketConnecting != true ) {
+              this.webSocketConnecting = true;
+              console.log('Connect to websocket ...');
+              this.openWebsocketConnection();
+            } else {
+              console.log('Connection process still running!');
+            }
+          }
+        } else {
+            socketConnector.close();
+            this.webSocketConnecting = false;
+        }
+      },
+
+      openWebsocketConnection() {
+        this.showConnectBtnSpinner = true;
+
+        socketConnector.connect(this.ip).then(() => {
+          this.webSocketReady = true;
+          this.showConnectBtnSpinner = false;
+          this.webSocketConnecting = false;
+        }).catch(() => {
+          this.showConnectBtnSpinner = false;
+          this.webSocketReady = false;
+          this.webSocketConnecting = false;
+
+          console.error("Could not connect to websocket " + this.ip + "!");
+        });
+      },
+
+      onSaveButtonClicked() {
+
       },
 
       emitPPMData() {
@@ -168,11 +294,21 @@
         this.$emit("data", this.ppmImageData);
 
         this.ppmImageData = "";
-      }
+      },
+
+      onClearButtonClicked() {
+
+      },
+
+      onRebootButtonClicked() {
+
+      },
     },
 
     computed: {
-
+      connectButtonIcon() {
+        return this.webSocketReady ? 'disconnect' : 'connect';
+      }
     },
 
     components: {
